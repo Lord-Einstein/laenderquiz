510c7ecb51d9dc0af6b69317e5dbbd416ae30755
Martin Huecklekemkes <martin.huecklekemkes@hsrw.org>
2021-04-27 19:20:15 +0200

    Merged website with map

--- script.js
+++ script.js
@@ -9,4 +9,38 @@ $(document).ready(function() {
         $('#quizbox').addClass("activeBox");
 
     })
+
+    function highlight(id) {
+        if (polygonSeries.getPolygonById(id).properties.hoverable == true) {
+            activateHighlight(id);
+        } else {
+            deactivateHighlight(id);
+        }
+    };
+
+    function activateHighlight(id) {
+        polygonSeries.getPolygonById(id).setState("highlight");
+    }
+
+    function deactivateHighlight(id) {
+        polygonSeries.getPolygonById(id).setState("default");
+    }
+
+    //Toggle germany
+    $("#DEU").on('click', function() {
+        polygonSeries.getPolygonById("DE").isActive = !polygonSeries.getPolygonById("DE").isActive;
+    });
+
+    $("#toggleHL").on('click', function() {
+        highlight("DE");
+    });
+
+    $("#activateHL").on('click', function() {
+        activateHighlight("DE");
+    });
+
+    $("#deactivateHL").on('click', function() {
+        deactivateHighlight("DE");
+    });
+
 })
