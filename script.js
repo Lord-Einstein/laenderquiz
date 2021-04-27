716857268c1fd25b510d66c5d2473f6b8e73b619
Martin Huecklekemkes <martin.huecklekemkes@hsrw.org>
2021-04-27 21:57:11 +0200

    Merged website with map
    - Added GUI functionality
    - Added map to website
    - Added footer buttonbar for testing
    - Merged changes

--- script.js
+++ script.js
@@ -1,6 +1,133 @@
 $(document).ready(function() {
 
-    //Alles wichtigen Klicks
+    //#region Init map
+
+    //Create theme
+    function am4themes_myTheme(target) {
+        if (target instanceof am4core.ColorSet) {
+            target.list = [
+                // 0 Outline color
+                am4core.color("#ffffff"),
+                // 1 Background color
+                am4core.color("#363945"),
+                // 2 Primary color
+                am4core.color("#672E3B"),
+                // 3 Hover color
+                am4core.color("#9e475b"),
+                // 4 Active color
+                am4core.color("#6395F2"),
+                // 5 Active hover color
+                am4core.color("#1258DC"),
+                // 6 Highlight state color
+                am4core.color("#88B04B")
+            ];
+        }
+    };
+
+    //Applying themes
+    am4core.useTheme(am4themes_animated);
+    am4core.useTheme(am4themes_myTheme);
+
+    // Create map instance
+    var chart = am4core.create("chartdiv", am4maps.MapChart);
+
+    // Create map instance
+    var chart = am4core.create(
+        document.getElementById("chartdiv"),
+        am4maps.MapChart
+    );
+
+    // Set map definition
+    chart.geodata = am4geodata_worldHigh;
+    chart.geodataNames = am4geodata_lang_DE;
+
+    // Set projection
+    chart.projection = new am4maps.projections.Miller();
+
+    // Create map polygon series
+    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+    // Make map load polygon (like country names) data from GeoJSON
+    polygonSeries.useGeodata = true;
+
+    //Set Background
+    chart.background.fill = chart.colors.getIndex(1);
+    chart.background.fillOpacity = 1;
+
+    // Configure series
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name} : {id}";
+    polygonTemplate.stroke = chart.colors.getIndex(0);
+    polygonTemplate.properties.strokeWidth = 1;
+    polygonTemplate.fill = chart.colors.getIndex(2);
+
+
+    //Select countries
+    polygonSeries.include = ["AD", "AL", "AT", "BA", "BE", "BG", "BY", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GB", "GR", "HR", "HU", "IE", "IS", "IT", "LT", "LU", "LV", "MD", "ME", "MK", "MT", "NL", "NO", "PL", "PT", "RO", "RS", "RU", "SE", "SI", "SK", "TR", "UA", "XK", "", ];
+
+    // Create hover state, set fill and stroke color, set float
+    var hoverState = polygonTemplate.states.create("hover");
+    hoverState.properties.fill = chart.colors.getIndex(3);
+    hoverState.properties.stroke = chart.colors.getIndex(3).lighten(0.6);
+    hoverState.properties.strokeWidth = 3;
+    hoverState.properties.dx = -1;
+    hoverState.properties.dy = -2;
+
+    //Default to no dropshadow
+    var shadow = polygonTemplate.filters.push(new am4core.DropShadowFilter);
+    shadow.opacity = 0.2;
+
+    // Slightly shift the shadow and make it more prominent on hover
+    var hoverShadow = hoverState.filters.push(new am4core.DropShadowFilter);
+    hoverShadow.opacity = 1;
+    hoverShadow.dx = 2;
+    hoverShadow.dy = 4;
+
+    //Fix hover outlines
+    polygonSeries.mapPolygons.template.events.on("over", function(event) {
+        event.target.zIndex = Number.MAX_VALUE;
+        event.target.toFront();
+    });
+
+    // Create active state
+    var activeState = polygonTemplate.states.create("active");
+    activeState.properties.fill = chart.colors.getIndex(4);
+
+    // Create an event to toggle "active" state, prints the name
+    polygonTemplate.events.on("hit", function(ev) {
+        ev.target.isActive = !ev.target.isActive;
+
+        // get object info
+        //console.log(ev.target.dataItem.dataContext.name);
+    })
+
+    // Create hover active state and set alternative fill color
+    var hoverActive = polygonTemplate.states.create("hoverActive");
+    hoverActive.properties.fill = chart.colors.getIndex(5);
+    hoverActive.properties.stroke = chart.colors.getIndex(5).lighten(0.6);
+
+    // Disable zoom and pan
+    var zoomLvl = 5.5;
+    chart.seriesContainer.draggable = false;
+    chart.seriesContainer.resizable = false;
+    chart.minZoomLevel = zoomLvl;
+    chart.maxZoomLevel = zoomLvl;
+
+    //Set initial position and zoom
+    chart.homeZoomLevel = zoomLvl;
+    chart.homeGeoPoint = {
+        latitude: 56,
+        longitude: 7,
+    };
+
+    var highlight = polygonTemplate.states.create("highlight");
+    highlight.properties.fill = chart.colors.getIndex(6);
+    highlight.properties.hoverable = false;
+    highlight.properties.clickable = false;
+
+    //#endregion
+
+    //Alle wichtigen Klicks
     const Start = document.querySelector(".Start button");
     const Quizbox = document.querySelector(".Quizbox");
 
@@ -12,37 +139,42 @@ $(document).ready(function() {
      })
      */
 
-    function highlight(id) {
+    //#region Map Functions
+
+    function HL(id) {
         if (polygonSeries.getPolygonById(id).properties.hoverable == true) {
-            activateHighlight(id);
+            activateHL(id);
         } else {
-            deactivateHighlight(id);
+            deactivateHL(id);
         }
     };
 
-    function activateHighlight(id) {
+    function activateHL(id) {
         polygonSeries.getPolygonById(id).setState("highlight");
-    }
+    };
 
-    function deactivateHighlight(id) {
+    function deactivateHL(id) {
         polygonSeries.getPolygonById(id).setState("default");
-    }
+    };
+    //#endregion
 
+
+    //Buttons
     //Toggle germany
     $("#DEU").on('click', function() {
         polygonSeries.getPolygonById("DE").isActive = !polygonSeries.getPolygonById("DE").isActive;
     });
 
     $("#toggleHL").on('click', function() {
-        highlight("DE");
+        HL("DE");
     });
 
     $("#activateHL").on('click', function() {
-        activateHighlight("DE");
+        activateHL("DE");
     });
 
     $("#deactivateHL").on('click', function() {
-        deactivateHighlight("DE");
+        deactivateHL("DE");
     });
 
     //QUIZ-FUNKTIONEN
